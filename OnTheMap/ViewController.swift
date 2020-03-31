753c6ee016d63f0545684b11d55daed42d24c1c4
Sumair Zamir <sumair.zamir@gmail.com>
2020-03-31 22:05:24 +0100

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Aiman Nabeel on 31/03/2020.
+//  Copyright © 2020 Sumair Zamir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
